689286dc098480f66c183f4d91383f645f4c0591
Benjamin G. Wilson <you@example.com>
2020-12-07 23:07:13 +0000

    updated title setting in overtake_window

---
+++ find_possesive_well_laid_out_classes.py
@@ -0,0 +1,268 @@
+import tkinter as tk
+import tkinter.filedialog as tkfile
+from functools import partial
+import sqlite3
+from sqlite3 import Error
+import os.path
+from os import path
+
+class widget_variables:
+    def __init__(self, caller):
+        self.caller = caller
+
+class makers:
+    
+    def __init__(self, caller):
+        self.caller = caller
+        self.frame_padding = caller.frame_padding
+
+    def entries(self, inputs, frame): 
+    # populates a frame with entry spaces, returns a list of those entry spaces
+        
+        row_cnt = 0
+        for x in inputs.keys():
+            tk.Label(frame, text = x, anchor = "e").grid(row = row_cnt, column = 0, sticky = "NESW", padx = self.frame_padding, pady = self.frame_padding)
+            inputs[x] = tk.Entry(frame)
+            inputs[x].grid(row = row_cnt, column = 1, sticky = "NESW", padx = self.frame_padding, pady = self.frame_padding)
+            row_cnt = row_cnt + 1
+
+        return inputs
+
+    def file_prompts(self, file_prompts, frame): 
+    #populates a frame with file prompts, returns list of their linked StringVar()
+        
+        row_cnt = 0
+        for x in file_prompts.keys():
+            tk.Button(
+                frame, 
+                text = x,
+                command = file_prompts[x]
+            ).grid(row = row_cnt, column = 0, sticky = "E", padx = self.frame_padding, pady = self.frame_padding)
+            
+            file_prompts[x] = tk.StringVar(value = "...select file...")
+            
+            tk.Label(
+                frame, 
+                textvariable = file_prompts[x],
+                relief = tk.SUNKEN,
+                bg = "white"
+            ).grid(row = row_cnt, column = 1, sticky = "NESW", padx = self.frame_padding, pady = self.frame_padding)
+            
+            row_cnt = row_cnt + 1
+
+        return file_prompts 
+    
+    def button_panel(self, buttons, frame): 
+    # populates a frame with buttons, returns a list of those buttons
+        
+        col_cnt = 0
+        for x in buttons.keys():
+            buttons[x] = tk.Button(frame, text = x, command = buttons[x])
+            buttons[x].grid(row = 0, column = col_cnt, padx = self.frame_padding, pady = self.frame_padding)
+            col_cnt = col_cnt + 1
+
+        return buttons
+    
+    def make_new_database(self):
+        directory = self.dir_prompt_variables_for_widgets["Database Folder Location"].get()
+        filename = self.inputs["New Database Name"].get()
+
+        if path.isdir(directory) == False or len(filename) < 1:
+            return
+        
+        new_db = directory + "/" + filename + ".db"
+        
+        try:
+            db_obj = sqlite3.connect(new_db)
+            self.database_location = new_db
+            print(sqlite3.version)
+        except Error as e:
+            print(e)
+        finally:
+            if db_object:
+                db_obj.close()
+        
+    def make_dir_prompts(self, dir_prompts, frame): 
+    #populates a frame with dir prompts, returns list of their linked StringVar()
+        
+        row_cnt = 0
+        for x in dir_prompts.keys():
+            tk.Button(
+                frame, 
+                text = x,
+                command = dir_prompts[x]
+            ).grid(row = row_cnt, column = 0, sticky = "E", padx = self.frame_padding, pady = self.frame_padding)
+            
+            dir_prompts[x] = tk.StringVar(value = "...select directory...")
+            
+            tk.Label(
+                frame, 
+                textvariable = dir_prompts[x],
+                relief = tk.SUNKEN,
+                bg = "white"
+            ).grid(row = row_cnt, column = 1, sticky = "NESW", padx = self.frame_padding, pady = self.frame_padding)
+            
+            row_cnt = row_cnt + 1
+
+        return dir_prompts 
+    
+class dialogs:
+    
+    def __init__(self, caller):
+        self.caller = caller
+
+class Input_Window:
+    def __init__(self):
+        
+        self.frame_padding = 3
+        self.internal_col_width = 130
+
+        self.makers = makers(self)
+        self.widget_variables = widget_variables(self)
+        self.dialogs = dialogs(self)
+
+        self.master = tk.Tk()
+        self.master.title("Genital Counter")
+        self.master.columnconfigure(0, weight = 1)
+        
+
+
+        frame_text_inputs = tk.Frame()
+        frame_buttons = tk.Frame()
+        frame_file_prompts = tk.Frame()
+
+        frame_text_inputs.columnconfigure((0,1), minsize = self.internal_col_width, weight = 1)
+        frame_file_prompts.columnconfigure((0,1), minsize = self.internal_col_width, weight = 1)
+        frame_buttons.columnconfigure((0,1,2), weight = 1)
+
+        
+        inputs = {
+            "Title": None,
+            "Author": None,
+            "Author's Gender": None,
+            "Date": None,
+        }
+        
+        file_prompts ={
+            "File Location": self.set_file_location,
+            "Database Location" : self.set_database_location
+        }
+        
+        buttons = {
+            "About": None,
+            "Wiki": None,
+            "Submit": self.check_form_completion
+        }
+        
+        self.file_prompt_variables_for_widgets = self.makers.file_prompts(file_prompts, frame_file_prompts)
+        self.inputs = self.makers.entries(inputs, frame_text_inputs)
+        self.buttons = self.makers.button_panel(buttons, frame_buttons)
+
+        frame_text_inputs.grid(row = 0, column = 0, sticky = "NESW", padx = self.frame_padding, pady = self.frame_padding)
+        frame_file_prompts.grid(row = 1, column = 0, sticky = "NESW", padx = self.frame_padding, pady = self.frame_padding)
+        frame_buttons.grid(row = 2, column = 0, padx = self.frame_padding, pady = self.frame_padding)
+    
+    def set_file_location(self):
+        
+        path = tkfile.askopenfilename(
+            title = 'select', 
+            filetypes =[("*.txt", ".txt")]
+        )
+       
+        if len(path) > 0:
+            self.file_location = path 
+            self.file_prompt_variables_for_widgets["File Location"].set(path)
+    
+    def set_database_location(self):
+        path = tkfile.askopenfilename(
+            title = 'select', 
+            filetypes =[("*.db", ".db")]
+        )
+       
+        if len(path) > 0:
+            self.database_location = path 
+            self.file_prompt_variables_for_widgets["Database Location"].set(path)
+
+    def check_form_completion(self):
+    
+        for x in self.inputs:
+            if len(self.inputs[x].get()) < 1:
+                pop_up = tk.Toplevel(self.master)
+                pop_up.focus_force()
+                buttons = {
+                    "Yes": lambda:[pop_up.destroy(),self.does_file_exist()],
+                    "No": pop_up.destroy
+                    }
+                self.fill_pop_up("There are empty fields; do you want to continue?", buttons, pop_up)
+                break
+            else: 
+                self.does_file_exist
+    
+    def does_file_exist(self):
+        
+        if hasattr(self, "file_location") == False:
+            pop_up = tk.Toplevel(self.master)
+            pop_up.focus_force()
+            buttons = {
+                "Okay": pop_up.destroy
+                }
+            self.fill_pop_up("Please supply a file to analyse.", buttons, pop_up)
+        
+        elif hasattr(self, "database_location") == False:
+            pop_up = tk.Toplevel(self.master)
+            pop_up.focus_force()
+            buttons = {
+                "Add existing database": pop_up.destroy,
+                "Create new database": lambda: [pop_up.destroy, self.new_database_dialog()], 
+                "Skip database export": pop_up.destroy
+            }
+            self.fill_pop_up(
+                "You haven't supplied a database, what would you like to do? ", 
+                buttons, pop_up)            
+            
+    def fill_pop_up(self, text, buttons, window):
+        frame_buttons_warning = tk.Frame(window)
+        warning = tk.Label(window, text = text)
+        self.make_button_panel(buttons, frame_buttons_warning)
+        warning.grid(row = 0)
+        frame_buttons_warning.grid(row = 1)
+
+    def new_database_dialog(self):
+        pop_up = tk.Toplevel(self.master)
+        
+        frame1 = tk.Frame(pop_up)
+        frame2 = tk.Frame(pop_up)
+        frame3 = tk.Frame(pop_up)
+        
+        directory_requests = {
+            "Database Folder Location": self.ask_for_db_folder_dialog
+        }
+        inputs = {
+            "New Database Name": None
+        }
+        buttons = {
+            "Exit": pop_up.destroy,
+            "Create new database":self.make_new_database
+        }
+
+        self.dir_prompt_variables_for_widgets = self.make_dir_prompts(directory_requests, frame1)
+        self.inputs.update(self.makers.entry(inputs, frame2))
+        self.buttons.update(self.make_button_panel(buttons, frame3))
+
+        frame1.grid(row = 0)
+        frame2.grid(row = 1)
+        frame3.grid(row = 2)
+
+
+    
+    def ask_for_db_folder_dialog(self):
+        
+        folder = tkfile.askdirectory(title = 'select')
+        self.dir_prompt_variables_for_widgets["Database Folder Location"].set(folder)
+
+def main():
+    
+    app = Input_Window()
+    app.master.mainloop()
+
+main()
---
+++ interface_class.py
@@ -0,0 +1,226 @@
+import tkinter as tk
+
+class panel:
+
+    def __init__(self):
+        
+        self.window = None
+
+        # visual styling defaults
+        self.title = ""
+        self.widget_pad = 1
+        self.frame_pad = 5
+        self.sticky = "nsew"
+        self.bg = "RosyBrown1"
+        self.col_a_width = 100  #
+        self.col_b_width = 200
+        self.text_wrap = 300
+        self.borderwidth = 2
+        self.relief = "ridge"
+        self.text_justify = tk.CENTER
+        self.column_weight = 1
+        self.row_weight = 1
+
+        # widgets on panel
+        self.legend = ""
+        self.entries = {}       
+        self.file_prompts = {}
+        self.dir_prompts = {}
+        self.buttons = {}
+
+        # containers for input data
+        self.variables = {}
+
+    def overtake_window(self, window):
+        window.title(self.title)
+        self.as_frames_in(window)
+
+    def as_frames_in(self, parent):
+        
+        self.window = parent
+        widget_frames = []
+        
+        if len(self.legend) > 0:
+            widget_frames.append(self.frame_legend(parent))
+        if len(self.entries) > 0:
+            widget_frames.append(self.frame_entries(parent))
+        if len(self.file_prompts) > 0:
+            widget_frames.append(self.frame_file_prompts(parent))
+        
+        row_cnt = 0
+        for x in widget_frames:
+            self.format(x)
+            x.grid(
+                row = row_cnt, 
+                column = 0, 
+                sticky = self.sticky,
+                padx = self.frame_pad,
+                pady = self.frame_pad
+            )
+            row_cnt = row_cnt + 1
+        
+        self.format(parent)
+
+    def as_popup_from(self, parent):
+        pop_up = tk.Toplevel(parent)
+        self.window = pop_up
+        
+        pop_up.title(self.title)
+        pop_up.lift()
+        self.as_frames_in(pop_up)
+        self.format(pop_up)
+        pop_up.focus()
+
+        
+
+    def frame_legend(self,parent_frame):
+
+        frame_legend = tk.Frame(parent_frame)
+        
+        lbl_legend = tk.Label(
+            frame_legend, 
+            text = self.legend,
+            bg = self.bg,
+            wraplength = self.text_wrap,
+            justify = self.text_justify
+        )
+        
+        lbl_legend.grid(
+            row = 0,
+            sticky = self.sticky,
+            pady = self.widget_pad,
+            padx = self.widget_pad,
+        )
+
+        return frame_legend
+    
+    def frame_entries(self, parent_frame):
+        
+        frame_entries = tk.Frame(parent_frame)
+        frame_entries.columnconfigure(0, weight = 1, minsize = self.col_a_width)
+        frame_entries.columnconfigure(1, weight = 1, minsize = self.col_b_width)
+        row_cnt = 0
+            
+        for x in self.entries:
+            
+            label = tk.Label(
+                frame_entries, 
+                text = x, 
+                anchor = "e",
+                bg = self.bg
+            )
+
+            entry = tk.Entry(
+                frame_entries
+            )
+
+            label.grid(
+                row = row_cnt, 
+                column = 0, 
+                sticky = self.sticky, 
+                padx = self.widget_pad, 
+                pady = self.widget_pad
+            )
+
+            entry.grid(
+                row = row_cnt,
+                column = 1,
+                sticky = self.sticky,
+                padx = self.widget_pad,
+                pady = self.widget_pad
+            )
+            
+            row_cnt = row_cnt + 1
+        
+        return frame_entries
+    
+    def frame_file_prompts(self, parent_frame):
+        
+        frame_file_prompts = tk.Frame(parent_frame)
+        frame_file_prompts.columnconfigure(0, minsize = self.col_a_width)
+        frame_file_prompts.columnconfigure(1, minsize = self.col_b_width)
+        row_cnt = 0
+    
+        for x in self.file_prompts.keys():
+            button = tk.Button(
+                frame_file_prompts, 
+                text = x,
+                command = self.file_prompts[x],
+                bg = self.bg
+            )
+            button.grid(
+                row = row_cnt, 
+                column = 0, 
+                sticky = self.sticky, 
+                padx = self.widget_pad, 
+                pady = self.widget_pad
+            )
+            label = tk.Label(
+                frame_file_prompts, 
+                textvariable = self.file_prompts[x],
+                relief = tk.SUNKEN,
+            )
+            label.grid(
+                row = row_cnt, 
+                column = 1, 
+                sticky = self.sticky, 
+                padx = self.widget_pad, 
+                pady = self.widget_pad,
+            )
+            row_cnt = row_cnt + 1
+        
+        return frame_file_prompts
+
+    def format(self, frame):
+        
+        frame.configure(
+            padx = self.frame_pad,
+            pady = self.frame_pad,
+            bg = self.bg,
+            borderwidth = self.borderwidth,
+            relief = self.relief,
+        )
+
+        numcols, numrows = frame.grid_size()
+
+        for i in range(numcols):
+            frame.columnconfigure(i, weight = self.column_weight)
+                    
+        for i in range(numrows):
+            frame.rowconfigure(i, weight = self.row_weight)
+
+
+if __name__ == "__main__":
+    
+    window = tk.Tk()
+    window.lift()
+
+    
+    window_widgets = panel()
+    window_widgets.title = "Main Window"
+    window_widgets.legend = (
+        "this is the root frame. it should be all one colour, resizable in both directions,"
+        " and have no visual problems"
+    ) 
+    window_widgets.entries =[
+        "Entry"
+    ]
+    window_widgets.file_prompts = {
+        "File Prompt": None
+    }
+    window_widgets.dir_prompts = {
+        "Directory Prompt": None
+    }
+
+    
+    
+    red_popup = panel()
+    red_popup.title = "frame in red"
+    red_popup.bg = "red"
+    red_popup.legend = "This pop up tests how things look on the pop up"
+    red_popup.entries = {"Entry"}
+
+    red_popup.as_popup_from(window)  
+    
+    window_widgets.overtake_window(window)
+    window.mainloop()
