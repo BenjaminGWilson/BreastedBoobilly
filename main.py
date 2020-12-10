2ee9b7e232e36357a1b03d81c7479299b8116e88
Benjamin G. Wilson <you@example.com>
2020-12-10 22:38:35 +0000

    moved all panel/tkinter funcs into main

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
+# This file contains all information needed for launching the interface.
+# SQL and line splitting functions are in ________________
+
 import tkinter as tk
+from functools import partial 
 
 from Panel_class import Panel
-import panels as p
+
+
+def input_panel():
+
+    input_panel = Panel()
+    input_panel.title = "Genital Counter"
+    input_panel.entries = [
+        "Title",
+        "Author",
+        "Author's Gender",
+        "Date",
+    ]
+    input_panel.file_prompts = {
+        "Text location": (("text file","*.txt"),("all files","*.*")),
+        "Database location": (("database file", "*db"),)
+    }
+    input_panel.buttons = {
+        "About": partial(about().as_popup_from, root),
+        "Wiki": None,
+        "Submit": None
+    }
+    return input_panel
+
+def about():
+    about = Panel()
+    about.title = "About"
+    about.legend = ("This is some gumph about the program"
+                "and all the things it is etc etc")
+    return about
 
 root = tk.Tk()
-
-main = p.main_input()
-main.overtake_window(root)
-
+main_input_window = input_panel()
+main_input_window.overtake_window(root)
 root.mainloop()
