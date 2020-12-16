157a9cc312172c0694574687a8530add2785a22a
Benjamin G. Wilson <you@example.com>
2020-12-16 16:21:28 +0000

    init word_tally class

---
+++ text_analysis.py
@@ -0,0 +1,21 @@
+#define Word_tally class for returning final counts
+# receive user input + tkvariable for updating display (maybe 2, one that can be the words being processed?)
+# check that db, if supplied, has correct tables (add them if not)
+# load and test dictionaries
+# function that turns novel into tuples of ownership
+# function that counts tuples for each possesive 
+# and returns word_tallys[]: total objects, total body, total internal, total external, total each area
+
+class Word_tally(self)
+    def __init__():
+        self.total_owned = 0
+        self.total_body = 0
+        self.internal = 0
+        self.feet = 0
+        self.legs = 0
+        self.hands = 0
+        self.arms = 0
+        self.nethers = 0
+        self.torso = 0
+        self.head = 0
+        self.other = 0
